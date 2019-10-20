1afe7fb7b8917156612470555686e01ed0aabe46
Olga Grigoreva <gritty_olga@mail.ru>
2019-10-20 12:31:52 +0300

    Добавлены пропсы и валидация пропсов

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import App from "../src/components/app/app.jsx";
 
 const init = () => {
   ReactDOM.render(
-      <App />,
+      <App
+        cardNames = { [`Beautiful & luxurious apartment at great location`, `Wood and stone place`, `Canal View Prinsengracht`, `Nice, cozy, warm big bed apartment`, `Wood and stone place`]}
+      />,
       document.querySelector(`#root`)
   );
 };
